6237df5a508ad37c0c85affe0758a780f2061459
“Winiarski <winiarski.a.r@gmail.com>
2017-11-05 23:18:41 +0100

    Zadanie 19.1 Uaktualnienie projektu frontend Github v003

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  var apiRoot = 'https://salty-dawn-17007.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://salty-dawn-17007.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
